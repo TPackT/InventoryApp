115341a5bbb03a6255dee110e86115013ddf0a34
nguq03 <nguq03@vse.cz>
2024-12-14 01:56:08 +0100

    setup

---
+++ main.js
@@ -0,0 +1,6 @@
+import { app } from "./src/app.js"
+
+const port = 3000
+const server = app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+export const app = express()
+app.set("view engine", "ejs")
+
+
+app.get("/", async (req, res) => {
+    res.render("index")
+})
