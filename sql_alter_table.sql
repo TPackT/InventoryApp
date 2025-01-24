275df40e4c9fa8cf40858e3a2d909e0d6a189f3a
nguq03 <nguq03@vse.cz>
2025-01-24 15:52:05 +0100

    version 1

---
+++ sql_alter_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO PRODUCTS (barcode, name, "SIZE", description, price, quantity, created_at, updated_at)
+VALUES ('5449000006271', 'Fanta', '500ml', 'desc', 25, 60, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+SELECT * FROM PRODUCTS;
